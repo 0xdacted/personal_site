71d967af4c545254c9335714480cf3910ba7ce9e
Red <crabmarket56@gmail.com>
2023-05-08 14:42:42 +0900

    Add tailwind customization and start website layout

---
+++ src/pages/components/header.tsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <div>
+      <div>Home</div>
+      <div>
+        <div>Movies</div>
+        <div>Directors</div>
+        <div>Books</div>
+        <div>Authors</div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,21 @@ module.exports = {
   mode: 'jit',
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    colors: {
+      background: '#F4F4F4',
+      text: '#333333',
+      primary: '#0088CC',
+      secondary: '#B7E4C7'
+    },
+    fontFamily: {
+      sans: ['Merriweather', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [],
