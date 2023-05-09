9150380891f2f7b09894fa116a714f9682e17076
Red <crabmarket56@gmail.com>
2023-05-09 15:41:38 +0900

    Correct text color

--- src/pages/components/header.tsx
+++ src/pages/components/header.tsx
@@ -2,27 +2,27 @@ const Header = () => {
   return (
     <header className="flex items-center justify-between py-4 px-8">
       <div className="w-1/3">
-        <a className="text-2xl font-bold text-gray-800 hover:text-gray-600" href="/">
+        <a className="text-2xl text-text hover:text-gray-600" href="/">
           Home
         </a>
       </div>
       <nav className="w-1/3 flex items-center justify-between">
-        <a className="text-lg font-medium text-gray-600 hover:text-gray-800 mx-3" href="/movies">
+        <a className="text-lg font-medium text-text hover:text-gray-800 mx-3" href="/movies">
           Movies
         </a>
-        <a className="text-lg font-medium text-gray-600 hover:text-gray-800 mx-3" href="/directors">
+        <a className="text-lg font-medium text-text hover:text-gray-800 mx-3" href="/directors">
           Directors
         </a>
-        <a className="text-lg font-medium text-gray-600 hover:text-gray-800 mx-3" href="/books">
+        <a className="text-lg font-medium text-text hover:text-gray-800 mx-3" href="/books">
           Books
         </a>
-        <a className="text-lg font-medium text-gray-600 hover:text-gray-800 mx-3" href="/authors">
+        <a className="text-lg font-medium text-text hover:text-gray-800 mx-3" href="/authors">
           Authors
         </a>
       </nav>
       <div className="w-1/3 flex items-center justify-end">
         <div className="mr-8">
-          <a className="text-lg font-medium text-gray-600 hover:text-gray-800" href="/over-under-game">
+          <a className="text-lg font-medium text-text hover:text-gray-800" href="/over-under-game">
             Over/Under Game
           </a>
         </div>
