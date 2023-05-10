1242a3cd81529f974879403c8051d5661ea31a91
Red <crabmarket56@gmail.com>
2023-05-10 14:32:47 +0900

    Fix front_page spacing and refactor intro text into component

--- src/pages/components/header.tsx
+++ src/pages/components/header.tsx
@@ -2,27 +2,27 @@ const Header = () => {
   return (
     <header className="flex items-center justify-between py-4 px-8">
       <div className="w-1/3">
-        <a className="text-lg text-text hover:text-gray-600" href="/">
+        <a className="text-xl text-text hover:text-gray-600" href="/">
           Home
         </a>
       </div>
       <nav className="w-1/3 flex items-center justify-between">
-        <a className="text-lg font-medium text-text hover:text-gray-800 mx-3" href="/movies">
+        <a className="text-xl font-medium text-text hover:text-gray-800 mx-3" href="/movies">
           Movies
         </a>
-        <a className="text-lg font-medium text-text hover:text-gray-800 mx-3" href="/directors">
+        <a className="text-xl font-medium text-text hover:text-gray-800 mx-3" href="/directors">
           Directors
         </a>
-        <a className="text-lg font-medium text-text hover:text-gray-800 mx-3" href="/books">
+        <a className="text-xl font-medium text-text hover:text-gray-800 mx-3" href="/books">
           Books
         </a>
-        <a className="text-lg font-medium text-text hover:text-gray-800 mx-3" href="/authors">
+        <a className="text-xl font-medium text-text hover:text-gray-800 mx-3" href="/authors">
           Authors
         </a>
       </nav>
       <div className="w-1/3 flex items-center justify-end">
         <div className="mr-8">
-          <a className="text-lg font-medium text-text hover:text-gray-800" href="/over-under-game">
+          <a className="text-xl font-medium text-text hover:text-gray-800" href="/over-under-game">
             Over/Under Game
           </a>
         </div>
--- src/pages/front_page/front_page.tsx
+++ src/pages/front_page/front_page.tsx
@@ -1,16 +1,12 @@
-import Header from '../components/header'
+import Header from '../components/Header'
+import IntroText from './front_page_components/IntroText'
 
 const FrontPage = () => {
 
   return (
     <div>
-      <Header></Header>
-      <div className='text-2xl'>
-      <p>Hi.</p>
-      <p>My name is Cody, I am a full-stack developer passionate about movies, books and code.</p>
-      <p>This website is a continuously updating passion project meant to help you learn more about me.</p>
-      <p>I hope you have as much fun exploring it as I did making it.</p>
-    </div>
+      <Header />
+      <IntroText />
     </div>
   )
 }
